2ccafd12c5029a488c1a8c9c25f5092c633775ab
tt <s17057@kaetsu.ac.jp>
2019-10-08 15:03:28 +0900

    adminページでCSSが適応されるようにした

--- django_site/settings.py
+++ django_site/settings.py
@@ -29,7 +29,7 @@ env.read_env(env_file)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.append(env('MYSERVER_URL'))
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
