21d1c7cbe3942ca5f98353716914cfdb913842ec
K1-scuba_diving <tellurium38@gmail.com>
2019-10-10 01:09:01 +0900

    djangoのログを保存することができるようになった

--- django_site/local_settings.py
+++ django_site/local_settings.py
@@ -10,3 +10,41 @@ DATABASES = {
             'NAME' : os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+LOGGING = {
+    # バージョンは「１」固定
+    'version' : 1,
+    # 既存のログ設定を無効化しない
+    'disable_existing_loggers' : False,
+    # ログフォーマット
+    'formatters': {
+        # 本番用
+        'production': {
+            'format' : '%(asctime)s [%(levelname)s] %(process)d %(thread)d'
+                        '%(pathname)s:%(lineno)d %(message)s'
+        },
+    },
+    # ハンドラ
+    'handlers':{
+        # ファイル出力用ハンドラ
+        'file' : {
+            'level':'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
+            'formatter': 'production',
+        },
+    },
+    'loggers': {
+        # 自作アプリケーション全般のログを拾うロガー
+        '' : {
+            'handlers' : ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        # Django本体が出すログ全般を拾うロガー
+        'django' : {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
